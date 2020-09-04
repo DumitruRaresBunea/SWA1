f362751462809f6f29107d5174027758b692d7ee
drb <266983@viauc.dk>
2020-09-04 10:00:04 +0200

    first commit

---
+++ ex1.2/Circle.js
@@ -0,0 +1,29 @@
+import Point from "./Point";
+
+const Circle = function (center, radius) {
+  this.center = center;
+  this.radius = radius;
+};
+
+Circle.prototype.getCenter = function () {
+  return this.center;
+};
+
+Circle.prototype.getRadius = function () {
+  return this.radius;
+};
+
+Circle.prototype.moveTo = function (x, y) {
+  this.getCenter = this.center.moveTo(x, y);
+};
+
+Circle.prototype.toString = function () {
+  return (
+    "center is at: " +
+    this.center.toString() +
+    " with a radius of: " +
+    this.radius
+  );
+};
+
+export default Circle;
---
+++ ex1.2/Point.js
@@ -0,0 +1,26 @@
+const Point = function (x, y) {
+  this.x = x;
+  this.y = y;
+};
+Point.prototype.x = function () {
+  return this.x;
+};
+
+Point.prototype.y = function () {
+  return this.y;
+};
+
+Point.prototype.moveTo = function (x, y) {
+  this.x = x;
+  this.y = y;
+};
+
+Point.prototype.toString = function () {
+  return "x is: " + this.x + ", y is: " + this.y;
+};
+
+Point.prototype.copy = function () {
+  return this;
+};
+
+export default Point;
