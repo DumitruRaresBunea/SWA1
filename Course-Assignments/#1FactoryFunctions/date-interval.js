9e309ec0600d2e905c95350f4a166f9216941e2d
drb <266983@viauc.dk>
2020-09-13 16:41:23 +0200

    Date interval class

---
+++ Course-Assignments/#1FactoryFunctions/date-interval.js
@@ -0,0 +1,43 @@
+const DateInterval = (startDate, endDate) => {
+  const dateInterval = {
+    startDate,
+    endDate,
+  };
+
+  dateInterval.from = () => startDate;
+  dateInterval.to = () => endDate;
+
+  dateInterval.contains = (date) => date > startDate && date < endDate;
+
+  return dateInterval;
+};
+
+// startdate1 = new Date(2014, 2, 3);
+// startdate2 = new Date(2015, 4, 6);
+// enddate1 = new Date(2020, 4, 8);
+// enddate2 = new Date(2020, 1, 8);
+// date1 = new Date(2016, 2, 3);
+// date2 = new Date(2021, 2, 3);
+
+// console.log(startdate1);
+// console.log(startdate2);
+// console.log(enddate1);
+// console.log(enddate2);
+// console.log(date1);
+// console.log(date2);
+
+// dateInterval1 = DateInterval(startdate1, enddate1)
+// dateInterval2 = DateInterval(startdate2, enddate2)
+
+// console.log(dateInterval1)
+// console.log(dateInterval2)
+
+// console.log(dateInterval1.contains(date1))
+// console.log(dateInterval1.contains(date2))
+
+// console.log(dateInterval2.contains(date1))
+// console.log(dateInterval2.contains(date2))
+
+
+
+
